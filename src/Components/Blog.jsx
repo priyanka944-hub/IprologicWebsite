bde01cb35c8566c47e52e38f9185c00f8ee9d287
priyanka944-hub <ypriyapriyanka1@gmail.com>
2023-09-04 21:12:49 +0530

    bloggrid images done

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -5,7 +5,7 @@ import image1 from "./Assets/blog-bottom-1.jpg";
 import image2 from "./Assets/blog-bottom-2.jpg";
 import image3 from "./Assets/blog-bottom-3.jpg";
 import image4 from "./Assets/blog-bottom-4.jpg";
-
+import * as Icon from "react-bootstrap-icons";
 
 export default function Blog() {
   return (
@@ -27,20 +27,84 @@ export default function Blog() {
       </div>
       <div className="bottom-blog">
         <div className="d-flex justify-content-center gap-5 p-5">
-          <div>
+          <div style={{ backgroundColor: "white" }}>
             <img src={image1} height={330} alt="blog-1" />
+            <div className="blog-cards">
+              <div className="d-flex justify-content-center align-items-center gap-2 blog-card-info">
+                <Icon.PencilSquare />
+                <span>Creativity</span>
+                <Icon.ChatLeft />
+                <span>5 Comments</span>
+                <Icon.Clock />
+                <span>28th January</span>
+              </div>
+              <h4>Improve design with typography?</h4>
+              <p>
+                Non illo quas blanditiis repellendus laboriosam minima animi.
+                Consectetur accusantium pariatur repudiandae!
+              </p>
+              <button className="btn btn-danger" style={{borderRadius:"20px"}}>Learn more</button>
+            </div>
           </div>
-          <div>
+          <div style={{ backgroundColor: "white" }}>
             <img src={image2} height={330} alt="blog-2" />
+            <div className="blog-cards">
+              <div className="d-flex justify-content-center align-items-center gap-2 blog-card-info">
+                <Icon.PencilSquare />
+                <span>Design</span>
+                <Icon.ChatLeft />
+                <span>5 Comments</span>
+                <Icon.Clock />
+                <span>28th January</span>
+              </div>
+              <h4>Interactivity connect consumer</h4>
+              <p>
+                Non illo quas blanditiis repellendus laboriosam minima animi.
+                Consectetur accusantium pariatur repudiandae!
+              </p>
+              <button className="btn btn-danger" style={{borderRadius:"20px"}}>Learn more</button>
+            </div>
           </div>
         </div>
         <div className="d-flex justify-content-center gap-5 p-5">
-        <div>
-        <img src={image3} height={330} alt="blog-3" />
-      </div>
-      <div>
-        <img src={image4} height={330} alt="blog-4" />
-      </div>
+          <div style={{ backgroundColor: "white" }}>
+            <img src={image3} height={330} alt="blog-3" />
+            <div className="blog-cards">
+              <div className="d-flex justify-content-center align-items-center gap-2 blog-card-info">
+                <Icon.PencilSquare />
+                <span>Community</span>
+                <Icon.ChatLeft />
+                <span>5 Comments</span>
+                <Icon.Clock />
+                <span>28th January</span>
+              </div>
+              <h4>Marketing Strategy to bring more affect</h4>
+              <p>
+                Non illo quas blanditiis repellendus laboriosam minima animi.
+                Consectetur accusantium pariatur repudiandae!
+              </p>
+              <button className="btn btn-danger" style={{borderRadius:"20px"}}>Learn more</button>
+            </div>
+          </div>
+          <div style={{ backgroundColor: "white" }}>
+            <img src={image4} height={330} alt="blog-4" />
+            <div className="blog-cards">
+              <div className="d-flex justify-content-center align-items-center gap-2 blog-card-info">
+                <Icon.PencilSquare />
+                <span>Marketing</span>
+                <Icon.ChatLeft />
+                <span>5 Comments</span>
+                <Icon.Clock />
+                <span>28th January</span>
+              </div>
+              <h4>Marketing Strategy to bring more affect</h4>
+              <p>
+                Non illo quas blanditiis repellendus laboriosam minima animi.
+                Consectetur accusantium pariatur repudiandae!
+              </p>
+              <button className="btn btn-danger" style={{borderRadius:"20px"}}>Learn more</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
