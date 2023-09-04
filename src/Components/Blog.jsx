10ca3d35cb920d7ab372f4c98effb897515c3c5e
priyanka944-hub <ypriyapriyanka1@gmail.com>
2023-09-04 20:42:00 +0530

    added blog grid page

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./CSS/blog.scss";
+import { Link } from "react-router-dom";
+import image1 from "./Assets/blog-bottom-1.jpg";
+import image2 from "./Assets/blog-bottom-2.jpg";
+import image3 from "./Assets/blog-bottom-3.jpg";
+import image4 from "./Assets/blog-bottom-4.jpg";
+
 
 export default function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className="blog-home">
+      <div className="blog-container">
+        <div className="background-blog">
+          <p className="line-bright">Our blog</p>
+          <h1 className="line-bright">Blog Articles</h1>
+          <div>
+            <Link
+              className="line-bright"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              Home
+            </Link>
+            <span className="line-bright"> / Our blog</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-blog">
+        <div className="d-flex justify-content-center gap-5 p-5">
+          <div>
+            <img src={image1} height={330} alt="blog-1" />
+          </div>
+          <div>
+            <img src={image2} height={330} alt="blog-2" />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center gap-5 p-5">
+        <div>
+        <img src={image3} height={330} alt="blog-3" />
+      </div>
+      <div>
+        <img src={image4} height={330} alt="blog-4" />
+      </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,28 +1,19 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-import './CSS/hom.scss'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./CSS/hom.scss";
 export default function Home() {
   return (
-    <div className='mainHome'>
+    <div className="mainHome">
+      <div className="Home">
+        <div className="head">
+          <h1>Our work is</h1>
+          <h1>presentation of our</h1>
+          <h1>capabilities.</h1>
 
-    <div className='Home'>
-    
-      <div className='head'>
-      <h1>Our work is</h1>
-      <h1>presentation of our</h1>
-      <h1>capabilities.</h1>
-
-      <button className='btn'>Get Started > </button>
+          <button className="btn">Get Started {">"}</button>
+        </div>
       </div>
-
-
     </div>
-
-
-    </div>
-    
-    
-    )
+  );
 }
